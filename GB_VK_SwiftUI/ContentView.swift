97622db7e6870762023f6e6630139051e2e2d2e3
Vitaliy Viktorovich <po4tavela@gmail.com>
2022-01-23 18:41:18 +0300

    init

---
+++ GB_VK_SwiftUI/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  GB_VK_SwiftUI
+//
+//  Created by Vitaliy Talalay on 22.01.2022.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ GB_VK_SwiftUI/GB_VK_SwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  GB_VK_SwiftUIApp.swift
+//  GB_VK_SwiftUI
+//
+//  Created by Vitaliy Talalay on 23.01.2022.
+//
+
+import SwiftUI
+
+@main
+struct GB_VK_SwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
